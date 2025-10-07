04b42bc47887f90db86dbfdde7ba472f668e8e40
Julien ROUSSEAU <jrousseau@doubletrade.com>
2025-10-07 14:58:28 +0200

    chore: delete dist from the release commit

--- dist/checker.d.ts
+++
@@ -1,3 +0,0 @@
-import ts from 'typescript';
-import type { TS, PluginOptions } from './types';
-export declare function runChecksOnSourceFile(ts: TS, program: ts.Program, sf: ts.SourceFile, opts: PluginOptions): ts.Diagnostic[];
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import ts from 'typescript';
-import type { TS } from './types';
-declare function init(modules: {
-    typescript: TS;
-}): {
-    create: (info: ts.server.PluginCreateInfo) => ts.LanguageService;
-};
-export = init;
